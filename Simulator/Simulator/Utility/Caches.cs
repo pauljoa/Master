6e80b685f481f43c3bb5b27a865368e3427f425b
Paul Joakim <pjandreassen@gmail.com>
2019-07-05 16:46:44 +0200

    Consolidated cache loading methods

--- Simulator/Simulator/Utility/Caches.cs
+++ Simulator/Simulator/Utility/Caches.cs
@@ -18,10 +18,18 @@ namespace Simulator.Utility
 
         public static void Initialize()
         {
-            GetAlgorithmsFromRepository(@"C:\Users\PaulJoakim\Source\Repos\Master\AlgorithmRepository");
-            GetInterfacesFromRepository(@"C:\Users\PaulJoakim\Source\Repos\Master\InterfaceRepository");
-            GetModelsFromRepository(@"C:\Users\PaulJoakim\Source\Repos\Master\ModelRepository");
+            //Old Explicit methods
+            //GetAlgorithmsFromRepository(@"C:\Users\PaulJoakim\Source\Repos\Master\AlgorithmRepository");
+            //GetInterfacesFromRepository(@"C:\Users\PaulJoakim\Source\Repos\Master\InterfaceRepository");
+            //GetModelsFromRepository(@"C:\Users\PaulJoakim\Source\Repos\Master\ModelRepository");
+            //Consolidated into one method
+            GetAssembliesFromRepository(@"C:\Users\PaulJoakim\Source\Repos\Master\AlgorithmRepository", CacheType.Algorithm);
+            GetAssembliesFromRepository(@"C:\Users\PaulJoakim\Source\Repos\Master\InterfaceRepository", CacheType.Interface);
+            GetAssembliesFromRepository(@"C:\Users\PaulJoakim\Source\Repos\Master\ModelRepository", CacheType.Model);
+            
         }
+
+        #region DEPRECATED Repo Methods
         private static void GetModelsFromRepository(string path)
         {
             DirectoryInfo dir = new DirectoryInfo(@"" + path);
@@ -96,7 +104,57 @@ namespace Simulator.Utility
                 }
             }
         }
+        #endregion
+
+        /// <summary>
+        /// Loads all assemblies found in the given path, and adds them to the specified cache
+        /// </summary>
+        /// <param name="path">Directory path to </param>
+        /// <param name="cache"></param>
+        private static void GetAssembliesFromRepository(string path, CacheType cache)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"" + path);
+            FileInfo[] Files = dir.GetFiles("*.dll");
 
 
+            foreach (var file in Files)
+            {
+                var Dll = Assembly.LoadFrom(file.FullName);
+
+                try
+                {
+                    var typeList = Dll.GetExportedTypes();
+                    foreach (var type in typeList)
+                    {
+                        switch(cache)
+                        {
+                            case CacheType.Model:
+                                Models.Add(type.FullName.ToLower(), type);
+                                break;
+                            case CacheType.Interface:
+                                Interfaces.Add(type.FullName.ToLower(), type);
+                                break;
+                            case CacheType.Algorithm:
+                                Algorithms.Add(type.FullName.ToLower(), type);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+
+
+    }
+    enum CacheType
+    {
+        Model,
+        Interface,
+        Algorithm
     }
 }
